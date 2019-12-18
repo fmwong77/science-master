7e318c4f7fde4caff0168a6a379e62ada3374635
fmwong77 <fongmeiw@gmail.com>
2019-12-18 15:53:03 -0600

    created db schema

--- db/migrate/20191218205656_create_scores.rb
+++ db/migrate/20191218205656_create_scores.rb
@@ -1,7 +1,7 @@
 class CreateScores < ActiveRecord::Migration[6.0]
   def change
     create_table :scores do |t|
-      t.integer :user_id
+      # t.integer :user_id
       t.integer :score
       t.string :time
       t.references :user, null: false, foreign_key: true
--- db/migrate/20191218212239_create_answers.rb
+++ db/migrate/20191218212239_create_answers.rb
@@ -1,7 +1,7 @@
 class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
-      t.integer :question_id
+      # t.integer :question_id
       t.string :text
       t.string :answer
       t.references :question, null: false, foreign_key: true
